4851e63087c8c51cf97eae44e469b9c1d57abfc5
Dario <Dario@DESKTOP-DN39ONN>
2018-03-16 23:17:22 +0100

    Initial checkin of sales tool.

---
+++ SalseTool/src/ca/demo/salestool/Main.java
@@ -0,0 +1,17 @@
+package ca.demo.salestool;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+
+		_displayGreeting();
+		data.display();
+	}
+
+	private static void _displayGreeting() {
+		System.out.println("HELLO HAPPY SALES PEOPLE");
+		System.out.println("THIS APP SHOWS SALES DATA");
+	}
+
+}
---
+++ SalseTool/src/ca/demo/salestool/SalesData.java
@@ -0,0 +1,8 @@
+package ca.demo.salestool;
+
+public class SalesData {
+	
+	public void display() {
+		System.out.println("TODO: Implement data display");
+	}
+}
